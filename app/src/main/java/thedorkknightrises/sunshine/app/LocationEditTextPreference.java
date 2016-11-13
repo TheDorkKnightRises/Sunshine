90f8053f5ba225049e477aaf4cc034dc00369aa0
Samriddha Basu <samriddhabasu@gmail.com>
2016-11-14 04:47:50 +0530

    Initial commit

---
+++ app/src/main/java/thedorkknightrises/sunshine/app/LocationEditTextPreference.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package thedorkknightrises.sunshine.app;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class LocationEditTextPreference extends EditTextPreference {
+    static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+    private int mMinLength;
+
+    public LocationEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                thedorkknightrises.sunshine.app.R.styleable.LocationEditTextPreference,
+                0, 0);
+        try {
+            mMinLength = a.getInteger(thedorkknightrises.sunshine.app.R.styleable.LocationEditTextPreference_minLength, DEFAULT_MINIMUM_LOCATION_LENGTH);
+        } finally {
+            a.recycle();
+        }
+
+        // Check to see if Google Play services is available. The Place Picker API is available
+        // through Google Play services, so if this is false, we'll just carry on as though this
+        // feature does not exist. If it is true, however, we can add a widget to our preference.
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(getContext());
+        if (resultCode == ConnectionResult.SUCCESS) {
+            // Add the get current location widget to our location preference
+            setWidgetLayoutResource(thedorkknightrises.sunshine.app.R.layout.pref_current_location);
+        }
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        return super.onCreateView(parent);
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        EditText et = getEditText();
+        et.addTextChangedListener(new TextWatcher() {
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog d = getDialog();
+                if (d instanceof AlertDialog) {
+                    AlertDialog dialog = (AlertDialog) d;
+                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    // Check if the EditText is empty
+                    if (s.length() < mMinLength) {
+                        // Disable OK button
+                        positiveButton.setEnabled(false);
+                    } else {
+                        // Re-enable the button.
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ watchface/src/main/java/thedorkknightrises/sunshine/watchface/SunshineWatchFace.java
@@ -0,0 +1,456 @@
+package thedorkknightrises.sunshine.watchface;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.WindowInsets;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+import java.lang.ref.WeakReference;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+public class SunshineWatchFace extends CanvasWatchFaceService {
+    public static final String COLUMN_WEATHER_ID = "weather_id";
+    public static final String COLUMN_MIN_TEMP = "min";
+    public static final String COLUMN_MAX_TEMP = "max";
+    public static final String COLUMN_SHORT_DESC = "short_desc";
+    private static final Typeface NORMAL_TYPEFACE =
+            Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL);
+    private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(500);
+    /**
+     * Handler message id for updating the time periodically in interactive mode.
+     */
+    private static final int MSG_UPDATE_TIME = 0;
+
+    @Override
+    public Engine onCreateEngine() {
+        return new Engine();
+    }
+
+    private static class EngineHandler extends Handler {
+        private final WeakReference<Engine> mWeakReference;
+
+        public EngineHandler(SunshineWatchFace.Engine reference) {
+            mWeakReference = new WeakReference<>(reference);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            SunshineWatchFace.Engine engine = mWeakReference.get();
+            if (engine != null) {
+                switch (msg.what) {
+                    case MSG_UPDATE_TIME:
+                        engine.handleUpdateTimeMessage();
+                        break;
+                }
+            }
+        }
+    }
+
+    private class Engine extends CanvasWatchFaceService.Engine implements DataApi.DataListener,
+            GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        final Handler mUpdateTime = new EngineHandler(this);
+        boolean mRegisteredTimeZoneReceiver = false;
+        Paint mBackgroundPaint;
+        Paint mTextPaint, mSmallTmpText;
+        boolean mAmbient;
+        Calendar mCalendar;
+
+        final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mCalendar.setTimeZone(TimeZone.getDefault());
+                invalidate();
+            }
+        };
+        float mXOffset;
+        float mYOffset;
+        int maxTemp = 0, minTemp = 0;
+        int wearID = 0;
+        String desc = "Sunshine";
+
+        /**
+         * Whether the display supports fewer bits for each color in ambient mode. When true, we
+         * disable anti-aliasing in ambient mode.
+         */
+        boolean mLowBitAmbient;
+        private GoogleApiClient googleApiClient;
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+
+            setWatchFaceStyle(new WatchFaceStyle.Builder(SunshineWatchFace.this)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
+                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+                    .setShowSystemUiTime(false)
+                    .setAcceptsTapEvents(true)
+                    .build());
+            Resources resources = SunshineWatchFace.this.getResources();
+            mYOffset = resources.getDimension(R.dimen.digital_y_offset);
+
+            mBackgroundPaint = new Paint();
+            mBackgroundPaint.setColor(resources.getColor(R.color.background));
+
+            mTextPaint = new Paint();
+            mTextPaint = createTextPaint(resources.getColor(R.color.digital_text));
+
+            mSmallTmpText = new Paint();
+            mSmallTmpText = createTextPaint(resources.getColor(R.color.digital_text));
+
+            mCalendar = Calendar.getInstance();
+
+            //For Weather Update
+
+            googleApiClient = new GoogleApiClient.Builder(SunshineWatchFace.this)
+                    .addApi(Wearable.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+
+        @Override
+        public void onDestroy() {
+            mUpdateTime.removeMessages(MSG_UPDATE_TIME);
+            super.onDestroy();
+        }
+
+        private Paint createTextPaint(int textColor) {
+            Paint paint = new Paint();
+            paint.setColor(textColor);
+            paint.setTypeface(NORMAL_TYPEFACE);
+            paint.setAntiAlias(true);
+            return paint;
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+
+            if (visible) {
+                registerReceiver();
+
+                // Connect for receiving message from mobile
+                googleApiClient.connect();
+
+                // Update time zone in case it changed while we weren't visible.
+                mCalendar.setTimeZone(TimeZone.getDefault());
+                invalidate();
+            } else {
+                unregisterReceiver();
+            }
+            updateTimer();
+        }
+
+        private void registerReceiver() {
+            if (mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = true;
+            IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
+            SunshineWatchFace.this.registerReceiver(mTimeZoneReceiver, filter);
+        }
+
+        private void unregisterReceiver() {
+            if (!mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = false;
+            SunshineWatchFace.this.unregisterReceiver(mTimeZoneReceiver);
+            if (googleApiClient != null && googleApiClient.isConnected()) {
+                googleApiClient.disconnect();
+            }
+        }
+
+        @Override
+        public void onApplyWindowInsets(WindowInsets insets) {
+            super.onApplyWindowInsets(insets);
+            Resources resources = SunshineWatchFace.this.getResources();
+            boolean isRound = insets.isRound();
+            mXOffset = resources.getDimension(isRound
+                    ? R.dimen.digital_x_offset_round : R.dimen.digital_x_offset);
+            float textSize = resources.getDimension(isRound
+                    ? R.dimen.digital_text_size_round : R.dimen.digital_text_size);
+
+            mTextPaint.setTextSize(textSize);
+            mSmallTmpText.setTextSize(textSize / 3);
+        }
+
+        @Override
+        public void onPropertiesChanged(Bundle properties) {
+            super.onPropertiesChanged(properties);
+            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false);
+        }
+
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            invalidate();
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            if (mAmbient != inAmbientMode) {
+                mAmbient = inAmbientMode;
+                if (mLowBitAmbient) {
+                    mTextPaint.setAntiAlias(!inAmbientMode);
+                    mSmallTmpText.setAntiAlias(!inAmbientMode);
+                }
+                invalidate();
+            }
+            updateTimer();
+        }
+
+        /**
+         * Captures tap event (and tap type) and toggles the background color if the user finishes
+         * a tap.
+         */
+        @Override
+        public void onTapCommand(int tapType, int x, int y, long eventTime) {
+            switch (tapType) {
+                case TAP_TYPE_TOUCH:
+                    // The user has started touching the screen.
+                    break;
+                case TAP_TYPE_TOUCH_CANCEL:
+                    // The user has started a different gesture or otherwise cancelled the tap.
+                    break;
+                case TAP_TYPE_TAP:
+                    // The user has completed the tap gesture.
+                    break;
+            }
+            invalidate();
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            // Draw the background.
+            if (isInAmbientMode()) {
+                canvas.drawColor(Color.BLACK);
+            } else {
+                canvas.drawRect(0, 0, bounds.width(), bounds.height(), mBackgroundPaint);
+            }
+
+            long now = System.currentTimeMillis();
+            mCalendar.setTimeInMillis(now);
+
+            String text = String.format("%02d:%02d",
+                    mCalendar.get(Calendar.HOUR),
+                    mCalendar.get(Calendar.MINUTE));
+
+            canvas.drawText(text, 50, 140, mTextPaint);
+
+            float widthOfTime = mTextPaint.measureText(text);
+            float upperYOffset = mYOffset - mTextPaint.getTextSize();
+
+            int weatherId = getWeatherIcon(wearID);
+            if (weatherId == -1) {
+                Bitmap weatherIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_clear);
+                canvas.drawBitmap(weatherIcon, mXOffset + 10, mYOffset - 55, mTextPaint);
+            } else {
+                Bitmap weatherIcon = BitmapFactory.decodeResource(getResources(), weatherId);
+                canvas.drawBitmap(weatherIcon, mXOffset + 10, mYOffset - 55, mTextPaint);
+            }
+
+            canvas.drawText(maxTemp + " \u00b0" + "C", widthOfTime - 10, mYOffset - mSmallTmpText.getTextSize() + 10, mSmallTmpText);
+            canvas.drawText(minTemp + " \u00b0" + "C", widthOfTime - 10, mYOffset + 15, mSmallTmpText);
+
+
+            float baseTextYOffset = mYOffset + 30;
+            canvas.drawText(getDay(mCalendar.get(Calendar.DAY_OF_WEEK)) + ", " +
+                            mCalendar.get(Calendar.DAY_OF_MONTH) + " " +
+                            getMonth(mCalendar.get(Calendar.MONTH)) + " '" +
+                            (mCalendar.get(Calendar.YEAR) + " ").substring(2, 4)
+
+                    , 60, 170, mSmallTmpText);
+
+            canvas.drawLine(widthOfTime - 40, upperYOffset + 30, widthOfTime - 40, baseTextYOffset, mTextPaint);
+
+            if (!isInAmbientMode()) {
+            }
+        }
+
+
+        /**
+         * Starts the {@link #mUpdateTime} timer if it should be running and isn't currently
+         * or stops it if it shouldn't be running but currently is.
+         */
+        private void updateTimer() {
+            mUpdateTime.removeMessages(MSG_UPDATE_TIME);
+            if (shouldTimerBeRunning()) {
+                mUpdateTime.sendEmptyMessage(MSG_UPDATE_TIME);
+            }
+        }
+
+        /**
+         * Returns whether the {@link #mUpdateTime} timer should be running. The timer should
+         * only run when we're visible and in interactive mode.
+         */
+        private boolean shouldTimerBeRunning() {
+            return isVisible() && !isInAmbientMode();
+        }
+
+        /**
+         * Handle updating the time periodically in interactive mode.
+         */
+        private void handleUpdateTimeMessage() {
+            invalidate();
+            if (shouldTimerBeRunning()) {
+                long timeMs = System.currentTimeMillis();
+                long delayMs = INTERACTIVE_UPDATE_RATE_MS
+                        - (timeMs % INTERACTIVE_UPDATE_RATE_MS);
+                mUpdateTime.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs);
+            }
+        }
+
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+            Wearable.DataApi.addListener(googleApiClient, this);
+            Log.d("Sunshine", "Connected");
+        }
+
+
+        @Override
+        public void onDataChanged(DataEventBuffer dataEvents) {
+            Log.d("Data Change", "Called");
+            for (DataEvent event : dataEvents) {
+                if (event.getType() == DataEvent.TYPE_CHANGED) {
+                    DataItem item = event.getDataItem();
+                    if (item.getUri().getPath().compareTo("/wear") == 0) {
+                        DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                        if (dataMap.containsKey(COLUMN_MAX_TEMP))
+                            maxTemp = dataMap.getInt(COLUMN_MAX_TEMP);
+                        if (dataMap.containsKey(COLUMN_MIN_TEMP))
+                            minTemp = dataMap.getInt(COLUMN_MIN_TEMP);
+                        if (dataMap.containsKey(COLUMN_WEATHER_ID))
+                            wearID = dataMap.getInt("WEAR_ID");
+                        if (dataMap.containsKey(COLUMN_SHORT_DESC))
+                            desc = dataMap.getString(COLUMN_SHORT_DESC);
+                    }
+                }
+            }
+
+            dataEvents.release();
+            invalidate();
+            googleApiClient.disconnect();
+            googleApiClient.connect();
+        }
+
+
+        @Override
+        public void onConnectionSuspended(int i) {
+            Log.d("Connection", "Fail");
+        }
+
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+            Log.d("Connection", "Fail" + connectionResult.getErrorMessage());
+        }
+
+        String getMonth(int n) {
+            switch (n) {
+                case 1:
+                    return "Jan";
+                case 2:
+                    return "Feb";
+                case 3:
+                    return "Mar";
+                case 4:
+                    return "Apr";
+                case 5:
+                    return "May";
+                case 6:
+                    return "Jun";
+                case 7:
+                    return "Jul";
+                case 8:
+                    return "Aug";
+                case 9:
+                    return "Sept";
+                case 10:
+                    return "Oct";
+                case 11:
+                    return "Nov";
+                case 12:
+                    return "Dec";
+            }
+            return null;
+        }
+
+        String getDay(int n) {
+            switch (n) {
+                case 1:
+                    return "Sun";
+                case 2:
+                    return "Mon";
+                case 3:
+                    return "Tue";
+                case 4:
+                    return "Wed";
+                case 5:
+                    return "Thu";
+                case 6:
+                    return "Fri";
+                case 7:
+                    return "Sat";
+            }
+            return null;
+        }
+
+        int getWeatherIcon(int weatherId) {
+            if (weatherId >= 200 && weatherId <= 232) {
+                return R.drawable.ic_storm;
+            } else if (weatherId >= 300 && weatherId <= 321) {
+                return R.drawable.ic_light_rain;
+            } else if (weatherId >= 500 && weatherId <= 504) {
+                return R.drawable.ic_rain;
+            } else if (weatherId == 511) {
+                return R.drawable.ic_snow;
+            } else if (weatherId >= 520 && weatherId <= 531) {
+                return R.drawable.ic_rain;
+            } else if (weatherId >= 600 && weatherId <= 622) {
+                return R.drawable.ic_snow;
+            } else if (weatherId >= 701 && weatherId <= 761) {
+                return R.drawable.ic_fog;
+            } else if (weatherId == 761 || weatherId == 781) {
+                return R.drawable.ic_storm;
+            } else if (weatherId == 800) {
+                return R.drawable.ic_clear;
+            } else if (weatherId == 801) {
+                return R.drawable.ic_light_clouds;
+            } else if (weatherId >= 802 && weatherId <= 804) {
+                return R.drawable.ic_cloudy;
+            }
+            return -1;
+        }
+    }
+}
